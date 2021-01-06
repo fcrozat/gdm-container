969f3a3da5f6d68dce06078d69c0b51d9b559fe5
Frederic Crozat <fcrozat@suse.com>
2021-01-06 18:01:54 +0100

    improve deployment

--- create-container-podman.sh
+++ create-container-podman.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-
+echo downloading systemd service
 curl https://raw.githubusercontent.com/fcrozat/gdm-container/main/gdm.service >  /etc/systemd/system/display-manager.service
 systemctl daemon-reload
 
-podman play kube gdm.yml
+echo deploying container
+podman play kube --start=false https://raw.githubusercontent.com/fcrozat/gdm-container/main/gdm.yml
 
+echo ready, you can start gdm using
 echo systemctl start display-manager.service
