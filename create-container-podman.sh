2d6da4f2d852ccf2f184dc0556f4439174ece2e9
Frederic Crozat <fcrozat@suse.com>
2021-01-06 18:20:14 +0100

    fix pod container name

--- create-container-podman.sh
+++ create-container-podman.sh
@@ -5,7 +5,9 @@ curl https://raw.githubusercontent.com/fcrozat/gdm-container/main/gdm.service > 
 systemctl daemon-reload
 
 echo deploying container
-podman play kube --start=false https://raw.githubusercontent.com/fcrozat/gdm-container/main/gdm.yml
+curl https://raw.githubusercontent.com/fcrozat/gdm-container/main/gdm.yml > gdm.yml
+podman pod rm --ignore --force gdm_pod
+podman play kube --start=false ./gdm.yml
 
 echo ready, you can start gdm using
 echo systemctl start display-manager.service
