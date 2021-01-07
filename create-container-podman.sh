c3d2ecb4a5725e41bbd52f73a91902eaab2e0c83
Frederic Crozat <fcrozat@suse.com>
2021-01-07 14:38:53 +0100

    switch back to old way of creating container

--- create-container-podman.sh
+++ create-container-podman.sh
@@ -5,9 +5,20 @@ curl https://raw.githubusercontent.com/fcrozat/gdm-container/main/gdm.service > 
 systemctl daemon-reload
 
 echo deploying container
-curl https://raw.githubusercontent.com/fcrozat/gdm-container/main/gdm.yml > gdm.yml
-podman pod rm --ignore --force gdm_pod
-podman play kube --start=false ./gdm.yml
+# kubernetes yaml deployement doesn't work yet with our specific container
+#curl https://raw.githubusercontent.com/fcrozat/gdm-container/main/gdm.yml > gdm.yml
+#podman pod rm --ignore --force gdm_pod
+#podman play kube --start=false ./gdm.yml
+
+podman container create --replace --hostname gdm-container --name gdm_pod-gdm --network host --privileged \
+ --security-opt label=disable --userns=keep-id --pid host --user root:root --tz=local -v /dev:/dev:rslave \
+ -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -v /etc/shadow:/etc/shadow:ro \
+ -v /home:/home -v /run:/run:rslave -v /proc:/proc -v /sys/fs/cgroup:/sys/fs/cgroup -v /var/log:/var/log \
+ -v /etc/vconsole.conf:/etc/vconsole.conf:ro -v /etc/machine-id:/etc/machine-id:ro \
+ -v /etc/sysconfig:/etc/sysconfig:ro -v /etc/X11/xorg.conf.d:/etc/X11/xorg.conf.d:ro -v /tmp:/tmp \
+ -v /etc/locale.conf:/etc/locale.conf:ro \
+ registry.opensuse.org/home/fcrozat/branches/opensuse/templates/images/tumbleweed/containers/gdm-container:latest
+
 
 echo ready, you can start gdm using
 echo systemctl start display-manager.service
